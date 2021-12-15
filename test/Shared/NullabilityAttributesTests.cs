c496f8d18e88d8ff21177bc25212df58368907b3
Victor Nova <lostfreeman@gmail.com>
2021-12-14 17:26:53 -0800

    initial version

---
+++ test/Shared/NullabilityAttributesTests.cs
@@ -0,0 +1,33 @@
+﻿namespace System.Diagnostics.CodeAnalysis;
+
+class Test {
+    [AllowNull]
+    public string AllowNull {
+        get => allowNull;
+        set => allowNull = value ?? "42";
+    }
+    private string allowNull = "42";
+
+    [DisallowNull]
+    public string? DisallowNull {
+        get => disallowNull;
+        set => disallowNull = value ?? throw new ArgumentNullException(nameof(DisallowNull), "Cannot set to null");
+    }
+    string? disallowNull;
+
+    public string MemberNotNullAttribute { get; set; }
+    [MemberNotNull(nameof(MemberNotNullAttribute))]
+    void SetMemberNotNull() {
+        MemberNotNullAttribute = "42";
+    }
+
+    public Test() {
+        SetMemberNotNull();
+    }
+
+    public static void Main() {
+        var test = new Test();
+        // AllowNull
+        test.AllowNull = null;
+    }
+}
